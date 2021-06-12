2b4034cd9ce05fa0b617e0fd85501214bf803757
Dave Christenson <dave.christensonn@sharpspring.coom>
2021-06-11 21:37:17 -0500

    Show loading animation when loading large responses

--- client/src/components/Response.tsx
+++ client/src/components/Response.tsx
@@ -1,77 +1,98 @@
 import React from 'react';
 import Message from '../common/Message';
-import { Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
+import { Accordion, AccordionSummary, AccordionDetails, CircularProgress } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 type Props = {
 	message: Message,
 };
 const Response = ({ message }: Props) => {
+	const [responseBody, setResponseBody] = React.useState('');
+
 	const queryParams = getQueryParams(message);
+	getResponseBody(message)
+		.then((rb) => setResponseBody(rb));
 	return (
 		<div>
-			<div className={message.status < 300 ? '' : 'error'}>
-				<b>Status:&nbsp;</b>{message.status}
-			</div>
-			<div>
-				<b>Elapsed time:&nbsp;</b>{message.elapsedTime} ms
-			</div>
-			{ Object.keys(message.requestHeaders).length > 0 ?
-				< Accordion >
-					<AccordionSummary expandIcon={<ExpandMoreIcon />}>
-						<b>Request Headers:</b>
-					</AccordionSummary>
-					<AccordionDetails>
-						<pre>
-							{JSON.stringify(message.requestHeaders, null, 2)}
-						</pre>
-					</AccordionDetails>
-				</Accordion>
+			<React.Fragment>
+				<div className={message.status < 300 ? '' : 'error'}>
+					<b>Status:&nbsp;</b>{message.status}
+				</div>
+				<div>
+					<b>Elapsed time:&nbsp;</b>{message.elapsedTime} ms
+				</div>
+				{ Object.keys(message.requestHeaders).length > 0 ?
+					< Accordion >
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+							<b>Request Headers:</b>
+						</AccordionSummary>
+						<AccordionDetails>
+							<pre>
+								{JSON.stringify(message.requestHeaders, null, 2)}
+							</pre>
+						</AccordionDetails>
+					</Accordion>
+					: null}
+				{ Object.keys(message.responseHeaders).length > 0 ?
+					<Accordion>
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+							<b>Response Headers:</b>
+						</AccordionSummary>
+						<AccordionDetails>
+							<pre>
+								{JSON.stringify(message.responseHeaders, null, 2)}
+							</pre>
+						</AccordionDetails>
+					</Accordion>
+					: null}
+				{ queryParams ?
+					<Accordion>
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+							<b>Query Parameters:</b>
+						</AccordionSummary>
+						<AccordionDetails>
+							<pre>
+								{ JSON.stringify(queryParams, null, 2) }
+							</pre>
+						</AccordionDetails>
+					</Accordion>
 				: null}
-			{ Object.keys(message.responseHeaders).length > 0 ?
-				<Accordion>
-					<AccordionSummary expandIcon={<ExpandMoreIcon />}>
-						<b>Response Headers:</b>
-					</AccordionSummary>
-					<AccordionDetails>
-						<pre>
-							{JSON.stringify(message.responseHeaders, null, 2)}
-						</pre>
-					</AccordionDetails>
-				</Accordion>
-				: null}
-			{ queryParams ?
-				<Accordion>
-					<AccordionSummary expandIcon={<ExpandMoreIcon />}>
-						<b>Query Parameters:</b>
-					</AccordionSummary>
-					<AccordionDetails>
-						<pre>
-							{ JSON.stringify(queryParams, null, 2) }
-						</pre>
-					</AccordionDetails>
-				</Accordion>
-			: null}
 
-			<Accordion defaultExpanded={ true }>
-				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
-					<b title={JSON.stringify({ responseBody: message.responseBody }, null, 2)}>Response Data:</b>
-				</AccordionSummary>
-				<AccordionDetails>
-					<pre>
-						{getResponseBody(message)}
-					</pre>
-				</AccordionDetails>
-			</Accordion>
+				<Accordion defaultExpanded={true}>
+					<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+						<b>Response Data:</b>
+					</AccordionSummary>
+					<AccordionDetails>
+						<pre>
+							{responseBody}
+						</pre>
+					</AccordionDetails>
+				</Accordion>
+				{responseBody.length === 0 &&
+					<div style={{
+						width: '100%',
+						marginTop: '1rem',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+						<CircularProgress />
+					</div>
+				}
+			</React.Fragment>
 		</div>
 	);
-}
 
-function getResponseBody(message: Message): string {
-	if(typeof message.responseBody === 'object') {
-		return JSON.stringify(message.responseBody, null, 2)
-	} else {
-		return message.responseBody;
+	async function getResponseBody(message: Message): Promise<string> {
+		return new Promise<string>((resolve) => {
+			let response = '';
+			if(typeof message.responseBody === 'object') {
+				response = JSON.stringify(message.responseBody, null, 2);
+			} else {
+				response = message.responseBody;
+			}
+			setTimeout(() => resolve(response));
+		});
 	}
 }
 
