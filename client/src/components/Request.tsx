fedb2a6e79d7bf227761ef06e6d088d6461d0a58
Dave Christenson <dave.christensonn@sharpspring.coom>
2021-07-15 08:55:52 -0500

    Minor styling

--- client/src/components/Request.tsx
+++ client/src/components/Request.tsx
@@ -10,30 +10,33 @@ type Props = {
 };
 const Request = observer(({ isActive, onClick, store, onResend }: Props) => {
 	return (
-		<div className="request__msg-container">
-			<div className="request__msg-timestamp-container"
-				title={`sequence number: ${store.getMessage().sequenceNumber}`}>
-				<span className="request__msg-timestamp">{formatTimestamp(store.getMessage().timestamp)}</span>
-			</div>
-			<div className={`fa ${store.getIconClass()} request__msg-icon`}
-				title={store.getTooltip()}
-				style={{ cursor: 'pointer', float: 'left', color: store.getColor() }}
-				onClick={ onResend }
-			>
-			</div>
-			<div className={`fa ${isActive ? 'fa-caret-down' : 'fa-caret-right'} request__msg-caret`}>
-			</div>
-			<div className={`request__msg
-				${isActive ? ' active' : ''}
-				${store.isError() ? ' error' : ''}
-				${store.getVisited() && !store.isError() ? ' visited-color' : ''}
-				`}
-				title={store.getRequestBody()}
-				onClick={() => { onClick(); store.setVisited(true); } }
-			>
-				{store.getMessage().method+' '}
-				<b>{store.getMessage().endpoint+' '}</b>
-				({store.getMessage().clientIp+'->' + store.getMessage().serverHost}) {store.getUrl()}
+		<div>
+			<div className="request__msg-container">
+				<div className="request__msg-header">
+					<div className="request__msg-timestamp-container"
+						title={`sequence number: ${store.getMessage().sequenceNumber}`}>
+						<span className="request__msg-timestamp">{formatTimestamp(store.getMessage().timestamp)}</span>
+					</div>
+					<div className={`fa ${store.getIconClass()} request__msg-icon`}
+						title={store.getTooltip()}
+						style={{ cursor: 'pointer', float: 'left', color: store.getColor() }}
+						onClick={ onResend }
+					>
+					</div>
+					<div className={`request__msg
+						${isActive ? ' active' : ''}
+						${store.isError() ? ' error' : ''}
+						${store.getVisited() && !store.isError() ? ' visited-color' : ''}
+						`}
+						title={store.getRequestBody()}
+						onClick={() => { onClick(); store.setVisited(true); } }
+					>
+					<div className={`fa ${isActive ? 'fa-caret-down' : 'fa-caret-right'} request__msg-caret`}/>
+					{store.getMessage().method+' '}
+					<b>{store.getMessage().endpoint+' '}</b>
+					({store.getMessage().clientIp+'->' + store.getMessage().serverHost}) {store.getUrl()}
+					</div>
+				</div>
 			</div>
 			<div className="request__body" hidden={!isActive}>
 				{store.getRequestBody()}
