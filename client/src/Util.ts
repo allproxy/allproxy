62567bc214649dd7d075b7325edf08e225a1736f
Dave Christenson <dave.christenson@sharpspring.com>
2021-09-22 19:03:55 -0500

    BreakpointStore test

--- client/src/Util.ts
+++ client/src/Util.ts
@@ -3,13 +3,13 @@ import Message from './common/Message';
 export default class Util {
 
 	static fixNewlines(str: string) {
-        return (str as any).replaceAll('\\n', '\n') // fix up line breaks
-                  .replaceAll('\\', '')
-                  .replaceAll('""', '"'); // remove consecutive quotes
+        return str.replace(/\\n/g, '\n') // fix up line breaks
+                  .replace(/\\/g, '')
+                  .replace(/""/g, '"'); // remove consecutive quotes
 	}
 
     static isGraphQlError(message: Message) {
-        if ((message.url?.endsWith('/graphql') || message.url?.endsWith('/graphql-public')) 
+        if ((message.url?.endsWith('/graphql') || message.url?.endsWith('/graphql-public'))
             && Array.isArray(message.responseBody)) {
             for(const entry of message.responseBody) {
                 if(entry.errors) {
