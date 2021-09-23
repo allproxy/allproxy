dd81cf60b0e5cea8efd11b9a0ae9dfcc709b02e5
Dave Christenson <dave.christenson@sharpspring.com>
2021-09-22 21:08:08 -0500

    MessageStore test

---
+++ client/src/test/MessageStore.test.ts
@@ -0,0 +1,21 @@
+import MessageStore from '../store/MessageStore';
+import Message from '../common/Message';
+import ProxyConfig from '../common/ProxyConfig';
+
+test("MessageStore", () => {
+  const message = new Message();
+  message.clientIp = 'clientIp';
+  message.serverHost = 'serverHost';
+  message.url = '/url';
+  message.requestBody = 'requestBody';
+  message.proxyConfig = new ProxyConfig();
+  console.log(message);
+
+  const messageStore = new MessageStore(message);
+  expect(messageStore.getMessage()).toBe(message);
+
+  expect(messageStore.getRequestBody()).toBe('"requestBody"');
+  expect(messageStore.getUrl()).toBe('/url');
+  expect(messageStore.getRequestLine()).toBe('(clientIp->serverHost) /url');
+
+});
