56a76ded88cb87186e9ba5817c63996d9b506f8f
Dave Christenson <dave.christenson@sharpspring.com>
2021-09-24 09:48:54 -0500

    Use react-json package to end JSON request body

--- client/src/components/ResendModal.tsx
+++ client/src/components/ResendModal.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { Modal } from '@material-ui/core'
 import ResendStore from '../store/ResendStore';
+import ReactJson, { InteractionProps } from 'react-json-view';
 
 type Props = {
 	open: boolean,
@@ -8,6 +9,7 @@ type Props = {
 	store: ResendStore,
 };
 const ResendModal = observer(({ open, onClose, store }: Props) => {
+	const message = store.getMessage();
 	return (
 		<Modal
 			className="modal-window"
@@ -27,23 +29,37 @@ const ResendModal = observer(({ open, onClose, store }: Props) => {
 							<textarea className="resend-modal__url form-control" rows={2} cols={300}
 								onChange={ (e) => store.setMethodAndUrl(e.target.value) }
 								value={ store.getMethodAndUrl() }
-								placeholder="Enter <method> <url>"/>
+								placeholder="<method> <url>"/>
 						</div>
 						<div style={{marginTop: '10px'}}>
 							<label>Body:&nbsp;</label>
-							{/* <button className='resend-modal__indent-button btn btn-xs btn-primary'>
-								Indent
-							</button> */}
 						</div>
 						<div className="resend-modal__body-container">
-							<textarea className="resend-modal__body form-control" rows={10} cols={300}
-								onChange={ (e) => store.validateBody(e.target.value) }
-								value={ store.getBody() }
-								placeholder="Enter request body"/>
+							{typeof message.requestBody === 'object' && store.isBodyJson() ?
+								<ReactJson
+									src={message.requestBody}
+									name={false}
+									displayDataTypes={false}
+									quotesOnKeys={false}
+									onEdit={handleEdit}
+									onAdd={handleAdd}
+									onDelete={handleDelete}
+								/>
+							:
+								<textarea className="resend-modal__body form-control" rows={10} cols={300}
+									onChange={ (e) => store.setBody(e.target.value) }
+									value={ store.getBody() }
+									placeholder="Enter request body"/>
+						}
 	        	</div>
 	        </div>
 	        <div className="modal-footer">
 						<label className="resend-modal__error-message">{ store.getError() }</label>
+						<button type="button" className="settings-modal__cancel btn btn-default btn-default"
+							onClick={ onClose }
+						>
+							Cancel
+						</button>
 						<button type="button" className="resend-modal__send btn btn-default btn-success"
 							onClick={() => { store.doResend(); onClose() }}
 							disabled={ store.getError().length > 0 }
@@ -54,7 +70,22 @@ const ResendModal = observer(({ open, onClose, store }: Props) => {
 	      </div>
 	    </div>
 	  </Modal>
-	)
+	);
+
+	function handleEdit(props: InteractionProps) {
+		store.setBody(JSON.stringify(props.updated_src));
+		return true;
+	}
+
+	function handleAdd(props: InteractionProps) {
+		store.setBody(JSON.stringify(props.updated_src));
+		return true;
+	}
+
+	function handleDelete(props: InteractionProps) {
+		store.setBody(JSON.stringify(props.updated_src));
+		return true;
+	}
 })
 
 export default ResendModal;
