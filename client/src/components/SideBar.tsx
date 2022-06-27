0dc2a3f3b847a800980dd79b74cf8fdadd0f6766
Dave Christenson <Dave.Christenson@ibm.com>
2022-06-27 15:38:56 -0500

    Show User Agents in Side Bar

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -19,11 +19,26 @@ const SideBar = observer(() => {
 		}
 	};
 
+	const areAllUserAgentsSelected = (): boolean => {
+		const allUserAgents = filterStore.getSideBarUserAgents();
+		for (const userAgent of allUserAgents) if (!filterStore.isSideBarUserAgentChecked(userAgent)) return false;
+		return true;
+	}
+
+	const handleAllUserAgentChange = () => {
+		const allUserAgents = filterStore.getSideBarUserAgents();
+		if (areAllUserAgentsSelected()) {
+			allUserAgents.forEach((userAgent) => filterStore.setSideBarUserAgentChecked(userAgent, false));
+		} else {
+			allUserAgents.forEach((userAgent) => filterStore.setSideBarUserAgentChecked(userAgent, true));
+		}
+	};
+
 	return (
 		<div className="side-bar">
 			{
 				filterStore.getSideBarProtocolIconClasses().map((iconClass) => (
-					<div className="side-bar-protocol" key={iconClass}>
+					<div key={iconClass}>
 						<div className="side-bar-item">
 							<div className="side-bar-checkbox-icon">
 								<Checkbox className="side-bar-checkbox"
@@ -41,9 +56,13 @@ const SideBar = observer(() => {
 			}
 
 			{
+				(filterStore.getSideBarDomains().length > 0 || filterStore.getSideBarUserAgents().length > 0) &&
+				<hr className="side-bar-divider"></hr>
+			}
+
+			{
 				filterStore.getSideBarDomains().length > 0 &&
 				<div>
-					<hr className="side-bar-divider"></hr>
 					<div className="side-bar-item">
 						<Select className="side-bar-select"
 							multiple
@@ -68,6 +87,39 @@ const SideBar = observer(() => {
 										onChange={() => filterStore.toggleSideBarDomainChecked(domain)}
 									/>
 									<ListItemText primary={domain} />
+								</MenuItem>
+							))}
+						</Select>
+					</div>
+				</div>
+			}
+			{
+				filterStore.getSideBarUserAgents().length > 0 &&
+				<div>
+					<div className="side-bar-item">
+						<Select className="side-bar-select"
+							multiple
+							value={filterStore.getSideBarUserAgents()}
+							renderValue={() => "User Agents"}
+						>
+							<MenuItem
+								value="all"
+							>
+								<Checkbox className="side-bar-domain-checkbox"
+									checked={areAllUserAgentsSelected()}
+									onChange={handleAllUserAgentChange}
+								/>
+								<ListItemText
+									primary="Select All"
+								/>
+							</MenuItem>
+							{filterStore.getSideBarUserAgents().map((userAgent) => (
+								<MenuItem key={userAgent} value={userAgent}>
+									<Checkbox className="side-bar-domain-checkbox"
+										checked={filterStore.isSideBarUserAgentChecked(userAgent)}
+										onChange={() => filterStore.toggleSideBarUserAgentChecked(userAgent)}
+									/>
+									<ListItemText primary={userAgent} />
 								</MenuItem>
 							))}
 						</Select>
