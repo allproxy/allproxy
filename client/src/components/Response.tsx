ae2fc933c453602028981fc8bce30313ebaef515
Dave Christenson <Dave.Christenson@ibm.com>
2022-06-29 06:28:11 -0500

    Add chevron icon to close response panel

--- client/src/components/Response.tsx
+++ client/src/components/Response.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 import Message from '../common/Message';
-import { Accordion, AccordionSummary, AccordionDetails, CircularProgress } from '@material-ui/core';
+import { Accordion, AccordionSummary, AccordionDetails, CircularProgress, IconButton } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ReactJson from 'react-json-view';
 import MessageStore from '../store/MessageStore';
@@ -9,10 +9,11 @@ import { colorScheme } from '../App';
 type Props = {
 	message: Message,
 	store: MessageStore,
+	onClose: () => void,
 };
-const Response = ({ message, store }: Props) => {
+const Response = ({ message, store, onClose }: Props) => {
 	const LOADING = 'Loading...';
-	const [responseBody, setResponseBody] = React.useState<string | ReactElement<any,any>>(LOADING);
+	const [responseBody, setResponseBody] = React.useState<string | ReactElement<any, any>>(LOADING);
 
 	const queryParams = getQueryParams(message);
 	getResponseBody(message)
@@ -20,6 +21,9 @@ const Response = ({ message, store }: Props) => {
 	return (
 		<div>
 			<React.Fragment>
+				<IconButton onClick={onClose}>
+					<div className={"fa fa-chevron-right"} />
+				</IconButton>
 				<div className={message.status < 400 ? '' : 'error'}>
 					<b>Status:&nbsp;</b>{message.status}
 				</div>
@@ -29,15 +33,15 @@ const Response = ({ message, store }: Props) => {
 							<b>GRPC Status:&nbsp;</b>{store.getGrpcStatus()}
 						</div>
 						{store.getGrpcMessage().length > 0 && (
-						<div>
-							<b>GRPC Message:&nbsp;</b>{store.getGrpcMessage()}
-						</div>)}
+							<div>
+								<b>GRPC Message:&nbsp;</b>{store.getGrpcMessage()}
+							</div>)}
 					</div>
 				)}
 				<div>
 					<b>Elapsed time:&nbsp;</b>{message.elapsedTime} ms
 				</div>
-				{ Object.keys(message.requestHeaders).length > 0 ?
+				{Object.keys(message.requestHeaders).length > 0 ?
 					< Accordion >
 						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
 							<b>Request Headers:</b>
@@ -49,7 +53,7 @@ const Response = ({ message, store }: Props) => {
 						</AccordionDetails>
 					</Accordion>
 					: null}
-				{ Object.keys(message.responseHeaders).length > 0 ?
+				{Object.keys(message.responseHeaders).length > 0 ?
 					<Accordion>
 						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
 							<b>Response Headers:</b>
@@ -61,18 +65,18 @@ const Response = ({ message, store }: Props) => {
 						</AccordionDetails>
 					</Accordion>
 					: null}
-				{ queryParams ?
+				{queryParams ?
 					<Accordion>
 						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
 							<b>Query Parameters:</b>
 						</AccordionSummary>
 						<AccordionDetails>
 							<pre>
-								{ JSON.stringify(queryParams, null, 2) }
+								{JSON.stringify(queryParams, null, 2)}
 							</pre>
 						</AccordionDetails>
 					</Accordion>
-				: null}
+					: null}
 
 				<Accordion defaultExpanded={true}>
 					<AccordionSummary expandIcon={<ExpandMoreIcon />}>
@@ -100,17 +104,17 @@ const Response = ({ message, store }: Props) => {
 		</div>
 	);
 
-	async function getResponseBody(message: Message): Promise<string | ReactElement<any,any>> {
+	async function getResponseBody(message: Message): Promise<string | ReactElement<any, any>> {
 		return new Promise((resolve) => {
-			let response: string | ReactElement<any,any>;
-			if(typeof message.responseBody === 'object') {
+			let response: string | ReactElement<any, any>;
+			if (typeof message.responseBody === 'object') {
 				response = <ReactJson
-								theme={colorScheme ==='dark' ? 'google' : undefined}
-								src={message.responseBody}
-								name={false}
-								displayDataTypes={false}
-								quotesOnKeys={false}
-								/>;
+					theme={colorScheme === 'dark' ? 'google' : undefined}
+					src={message.responseBody}
+					name={false}
+					displayDataTypes={false}
+					quotesOnKeys={false}
+				/>;
 			} else {
 				response = message.responseBody;
 			}
@@ -119,16 +123,16 @@ const Response = ({ message, store }: Props) => {
 	}
 }
 
-function getQueryParams(message: Message): string[]|null {
+function getQueryParams(message: Message): string[] | null {
 	// Format query parameters
 	let queryParams: string[] = [];
-	if(message.url && message.url.indexOf('?') !== -1) {
-			var temp = message.url!.split('?')[1];
-			temp.split('&').forEach(function(param) {
-					var keyValue = param.split('=');
-					var value = keyValue.length > 1 ? unescape(keyValue[1]) : undefined;
-					queryParams.push(keyValue[0]+' = '+value);
-			})
+	if (message.url && message.url.indexOf('?') !== -1) {
+		var temp = message.url!.split('?')[1];
+		temp.split('&').forEach(function (param) {
+			var keyValue = param.split('=');
+			var value = keyValue.length > 1 ? unescape(keyValue[1]) : undefined;
+			queryParams.push(keyValue[0] + ' = ' + value);
+		})
 	}
 	return queryParams.length === 0 ? null : queryParams;
 }
