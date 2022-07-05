1bce7bd8d9e5986d85fa076437570e4cfe51ae7c
Dave Christenson <Dave.Christenson@ibm.com>
2022-07-05 15:40:56 -0500

    Side bar status code text wraps

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -227,7 +227,7 @@ const SideBar = observer(() => {
 				(filterStore.getSideBarStatuses().length > 0) && (
 					<div className="side-bar-item">
 						<div>
-							<div>Status Codes:</div>
+							<div style={{ whiteSpace: 'nowrap' }}>Status:</div>
 							<div style={{ display: 'flex' }}>
 								<Checkbox className="side-bar-checkbox"
 									size="small"
