359f75daed25b2404137a85216cc8b642c31c941
Dave Christenson <Dave.Christenson@ibm.com>
2022-07-31 18:09:16 -0700

    Remove hidden counts in Footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,5 @@
 import FilterStore from '../store/FilterStore';
-import SocketStore from '../store/SocketStore';
 import { observer } from 'mobx-react-lite';
-import MessageQueueStore from '../store/MessageQueueStore';
 import BreakpointStore from '../store/BreakpointStore';
 import ExcludeTags from './ExcludeTags';
 
@@ -9,38 +7,12 @@ import ExcludeTags from './ExcludeTags';
  * Footer view
  */
 type Props = {
-	socketStore: SocketStore,
-	messageQueueStore: MessageQueueStore,
 	filterStore: FilterStore,
 	breakpointStore: BreakpointStore
 };
-const Footer = observer(({ socketStore, messageQueueStore, filterStore, breakpointStore }: Props): JSX.Element => {
-
-	function getDisplayedCount(): number {
-		let n = 0;
-		messageQueueStore.getMessages().forEach((messageStore) => {
-			if (!filterStore.isFiltered(messageStore)) ++n;
-		})
-		return n;
-	}
-
+const Footer = observer(({ filterStore, breakpointStore }: Props): JSX.Element => {
 	return (
 		<div className="footer__container">
-			<div hidden>
-				<div className="footer__item" title="Received messages">
-					<div>{getDisplayedCount() + ' of ' + messageQueueStore.getTotalLength()}</div>
-				</div>
-			</div>
-			<div hidden>
-				<div className="footer__item" title="Received messages">
-					{socketStore.getRequestCount() > socketStore.getResponseCount() ??
-						<div>No Response: {socketStore.getRequestCount() - socketStore.getResponseCount()}</div>
-					}
-				</div>
-				<div className="footer__item" title="Messages queued at server">
-					<div>Queued: {socketStore.getQueuedCount()}</div>
-				</div>
-			</div>
 			<div>
 				<div className="footer__item" title="Number of active breakpoints">
 					<div>Breakpoints: {breakpointStore.getBreakpointCount()}</div>
