2c1684a6fd3615bb724f997beefafd59d0d61a47
Dave Christenson <Dave.Christenson@ibm.com>
2023-09-26 12:36:23 -0500

    JSON field extraction script updates

--- client/src/store/JSONLogSampleScript.js
+++ client/src/store/JSONLogSampleScript.js
@@ -1,13 +1,11 @@
-import { add } from "lodash";
-
-function sample(nonJson, jsonData) {
+const sample = function (preJSONString, jsonObject) {
     let date = new Date();
-    let level = jsonData && jsonData.level ? jsonData.level : 'info';
+    let level = jsonObject && jsonObject.level ? jsonObject.level : 'info';
     let category = '';
     let message = '';
-    if (jsonData.MESSAGE) message = jsonData.MESSAGE;
-    else if (jsonData.message) message = jsonData.message;
-    else if (jsonData.msg) message = jsonData.msg;
+    if (jsonObject.MESSAGE) message = jsonObject.MESSAGE;
+    else if (jsonObject.message) message = jsonObject.message;
+    else if (jsonObject.msg) message = jsonObject.msg;
 
     function parsePod(pod) {
         const podParts = pod.split('-');
@@ -17,30 +15,30 @@ function sample(nonJson, jsonData) {
         return podParts.join('-');
     }
 
-    if (jsonData._file) {
-        if (jsonData.msg_timestamp) {
-            date = new Date(jsonData.msg_timestamp);
-        } else if (jsonData._ts) {
-            date = new Date(jsonData._ts);
+    if (jsonObject._file) {
+        if (jsonObject.msg_timestamp) {
+            date = new Date(jsonObject.msg_timestamp);
+        } else if (jsonObject._ts) {
+            date = new Date(jsonObject._ts);
         }
 
-        if (jsonData.pod) {
-            category = parsePod(jsonData.pod);
-        } else if (jsonData._file) {
-            if (jsonData._host) {
-                category = jsonData._host + ' ';
+        if (jsonObject.pod) {
+            category = parsePod(jsonObject.pod);
+        } else if (jsonObject._file) {
+            if (jsonObject._host) {
+                category = jsonObject._host + ' ';
             }
-            category += parsePod(jsonData._file);
+            category += parsePod(jsonObject._file);
         }
     } else {
-        const tokens = nonJson.split(' ', 5);
+        const tokens = preJSONString.split(' ', 5);
         if (tokens.length >= 3) {
             date = new Date(tokens.slice(0, 3).join(' '));
         }
-        if (jsonData.msg_timestamp) {
-            date = new Date(jsonData.msg_timestamp);
-        } else if (jsonData._ts) {
-            date = new Date(jsonData._ts);
+        if (jsonObject.msg_timestamp) {
+            date = new Date(jsonObject.msg_timestamp);
+        } else if (jsonObject._ts) {
+            date = new Date(jsonObject._ts);
         }
         if (tokens.length >= 4) {
             let pod = tokens[3];
@@ -53,23 +51,23 @@ function sample(nonJson, jsonData) {
             category += parsePod(pod);
         }
     }
-    if (jsonData.Worker !== undefined) {
-        category += ' worker' + jsonData.Worker;
+    if (jsonObject.Worker !== undefined) {
+        category += ' worker' + jsonObject.Worker;
     }
 
     let additionalJSON = {};
 
-    if (Object.keys(jsonData).length === 0) {
-        const i = nonJson.indexOf('verb=');
+    if (Object.keys(jsonObject).length === 0) {
+        const i = preJSONString.indexOf('verb=');
         if (i !== -1) {
-            const keyValues = nonJson.substring(i).split(' ');
+            const keyValues = preJSONString.substring(i).split(' ');
             for (const kv of keyValues) {
                 const parts = kv.split('=');
                 additionalJSON[parts[0]] = parts[1];
             }
         }
-    } else if (category.indexOf('sys.journal') !== -1 && jsonData.message !== undefined) {
-        additionalJSON = JSON.parse(jsonData.message);
+    } else if (category.indexOf('sys.journal') !== -1 && jsonObject.message !== undefined) {
+        additionalJSON = JSON.parse(jsonObject.message);
         if (additionalJSON.msg !== undefined) message = additionalJSON.msg;
     }
 
--- client/src/store/JSONLogStore.ts
+++ client/src/store/JSONLogStore.ts
@@ -66,85 +66,22 @@ export class JSONLogField {
 
 const defaultScript =
 	`
-// Sample function used to extract level category and message from log entry
-// @param nonJson: string - non-JSON string
-// @param jsonData: {} - JSON data
-// @returns {date: <Date>, level: "error | warn | info", category: "category...",n message: "message...", additionalJSON: {}}
-function(nonJson, jsonData) {
-    let date = new Date();
-    let level = jsonData && jsonData.level ? jsonData.level : 'info';
-    let category = '';
-    let message = '';
-    if (jsonData.MESSAGE) message = jsonData.MESSAGE;
-    else if (jsonData.message) message = jsonData.message;
-    else if (jsonData.msg) message = jsonData.msg;
-
-    function parsePod(pod) {
-        const podParts = pod.split('-');
-        if (podParts.length > 1) {
-            podParts.pop();
-        }
-        return podParts.join('-');
-    }
-
-    if (jsonData._file) {
-        if (jsonData.msg_timestamp) {
-            date = new Date(jsonData.msg_timestamp);
-        } else if (jsonData._ts) {
-            date = new Date(jsonData._ts);
-        }
-
-        if (jsonData.pod) {
-            category = parsePod(jsonData.pod);
-        } else if (jsonData._file) {
-            if (jsonData._host) {
-                category = jsonData._host + ' ';
-            }
-            category += parsePod(jsonData._file);
-        }
-    } else {
-        const tokens = nonJson.split(' ', 5);
-        if (tokens.length >= 3) {
-            date = new Date(tokens.slice(0, 3).join(' '));
-        }
-        if (jsonData.msg_timestamp) {
-            date = new Date(jsonData.msg_timestamp);
-        } else if (jsonData._ts) {
-            date = new Date(jsonData._ts);
-        }
-        if (tokens.length >= 4) {
-            let pod = tokens[3];
-            if (pod.startsWith('mzone')) {
-                if (tokens.length >= 5) {
-                    pod = tokens[4];
-                    category = tokens[3] + ' ';
-                }
-            }
-            category += parsePod(pod);
-        }
-    }
-    if (jsonData.Worker !== undefined) {
-        category += ' worker' + jsonData.Worker;
-    }
-
-    let additionalJSON = {};
-
-    if (Object.keys(jsonData).length === 0) {
-        const i = nonJson.indexOf('verb=');
-        if (i !== -1) {
-            const keyValues = nonJson.substring(i).split(' ');
-            for (const kv of keyValues) {
-                const parts = kv.split('=');
-                additionalJSON[parts[0]] = parts[1];
-            }
-        }
-    } else if (category.indexOf('sys.journal') !== -1 && jsonData.message !== undefined) {
-        additionalJSON = JSON.parse(jsonData.message);
-        if (additionalJSON.msg !== undefined) message = additionalJSON.msg;
-    }
-
-    return { date, level, category, message, additionalJSON };
-}
+	// Function called to extract date, level, category and message
+	//
+	// @param preJSONString: string - optional non-JSON string proceeding JSON object
+	// @param jsonObject: {} - JSON log data
+	// @returns {date: Date, level: string, category: string, message: string, additionalJSON: {} }
+	//
+	// category is the pod name, process ID...
+	//
+	const jsonLogScript = function (preJSONString, jsonObject) {
+		let date = new Date();
+		let level = 'info';
+		let category = 'Test category';
+		let message = 'This is a test message';
+		let additionalJSON = {};
+		return { date, level, category, message, additionalJSON };
+	}
 `;
 
 export type LogEntry = {
@@ -207,8 +144,17 @@ export default class JSONLogStore {
 	}
 
 	public evalScript(script: string) {
+		let scriptNoComments = '';
+		for (const line of script.split('\n')) {
+			const lineTrim = line.trim();
+			if (lineTrim.length > 0 && !lineTrim.startsWith('//')) {
+				scriptNoComments += line;
+			}
+		}
+		const i = scriptNoComments.indexOf('function');
+		const func = scriptNoComments.substring(i);
 		let f = this.scriptFunc;
-		eval('f = ' + script);
+		eval('f = ' + func);
 		return f;
 	}
 
