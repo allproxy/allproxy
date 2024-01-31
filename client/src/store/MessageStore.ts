7999929c7ac972d3bf631ae0616cb5dc279e4d94
Dave Christenson <Dave.Christenson@ibm.com>
2024-01-31 08:48:12 -0600

    Safari doesn't work

--- client/src/store/MessageStore.ts
+++ client/src/store/MessageStore.ts
@@ -22,6 +22,18 @@ export default class MessageStore {
     private logEntry: LogEntry = { date: new Date(), level: '', category: '', appName: '', message: '', additionalJSON: {} };
 
     public constructor(message: Message, auto: boolean = false) {
+        for (const key in message.requestHeaders) {
+            if (key.toLowerCase() !== key) {
+                message.requestHeaders[key.toLowerCase()] = message.requestHeaders[key];
+                delete message.requestHeaders[key];
+            }
+        }
+        for (const key in message.responseHeaders) {
+            if (key.toLowerCase() !== key) {
+                message.requestHeaders[key.toLowerCase()] = message.responseHeaders[key];
+                delete message.responseHeaders[key];
+            }
+        }
         this.message = message;
         this.url = this.formatUrl(message.url!);
         this._isError = this.isErrorResponse(message);
