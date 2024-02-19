891e81d8b773e97f7ed7b3c12eaef0a2b718a969
Dave Christenson <Dave.Christenson@ibm.com>
2024-02-19 07:43:54 -0600

    Tabs for selecting file and pasting JSON

--- client/src/components/ImportJSONFileDialog.tsx
+++ client/src/components/ImportJSONFileDialog.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Dialog, DialogTitle, FormControl, FormControlLabel, ListItemText, MenuItem, Radio, RadioGroup, Select } from '@material-ui/core';
+import { Dialog, DialogTitle, FormControl, FormControlLabel, ListItemText, MenuItem, Radio, RadioGroup, Select, Tab, Tabs } from '@material-ui/core';
 import { mainTabStore } from '../store/MainTabStore';
 import { importJSONFile } from '../ImportJSONFile';
 import FileReaderStore from '../store/FileReaderStore';
@@ -8,6 +8,7 @@ import FileSubsetStore, { areSubsetsSupported, timeFieldName } from '../store/Fi
 import NewSubsetDialog from './NewSubsetDialog';
 import { dateToHHMMSS } from './Request';
 import CloseIcon from "@material-ui/icons/Close";
+import { TabContext, TabPanel } from '@material-ui/lab';
 
 //const bigFileSize = 1024 * 1024 * 1024; // 1G
 
@@ -30,6 +31,7 @@ const ImportJSONFileDialog = observer(({ open, onClose }: Props) => {
 	const [subsetSupported, setSubsetSupported] = React.useState(false);
 	const [startTime, setStartTime] = React.useState<string>("");
 	const [endTime, setEndTime] = React.useState<string>("");
+	const [tabValue, setTabValue] = React.useState<'1' | '2'>('1');
 
 	var input = document.createElement('input');
 	input.type = 'file';
@@ -37,17 +39,27 @@ const ImportJSONFileDialog = observer(({ open, onClose }: Props) => {
 	input.onchange = async (e: any) => {
 		const file = e.target.files[0] as File;
 		setSelectedFile(file);
-		const supported = await areSubsetsSupported(file.name);
-		if (supported) {
-			const { socketStore } = await import('../store/SocketStore');
-			setIsSorted(await socketStore.emitIsSorted(file.name, timeFieldName));
-			await fileSubsetStore.init(file.name);
-			if (fileSubsetStore.getSubsets().length > 0) {
-				setSelectedSubset(fileSubsetStore.getSubsets()[0].filterValue);
+		if (isGzip()) {
+			setIsSorted(undefined);
+			setSubsetSupported(false);
+		} else {
+			const supported = await areSubsetsSupported(file.name);
+			if (supported) {
+
+				const { socketStore } = await import('../store/SocketStore');
+				setIsSorted(await socketStore.emitIsSorted(file.name, timeFieldName));
+				await fileSubsetStore.init(file.name);
+				if (fileSubsetStore.getSubsets().length > 0) {
+					setSelectedSubset(fileSubsetStore.getSubsets()[0].filterValue);
+				}
 			}
+			setSubsetSupported(supported);
 		}
-		setSubsetSupported(supported);
 	};
+
+	function isGzip() {
+		return selectedFile && selectedFile.type.indexOf('gzip') !== -1;
+	}
 
 	function fileName(): string {
 		if (selectedFile === undefined) return '';
@@ -85,7 +97,7 @@ const ImportJSONFileDialog = observer(({ open, onClose }: Props) => {
 					}
 				} else {
 					const { socketStore } = await import('../store/SocketStore');
-					if (socketStore.isConnected() && await socketStore.emitIsFileInDownloads(fileName())) {
+					if (!isGzip() && socketStore.isConnected() && await socketStore.emitIsFileInDownloads(fileName())) {
 						await fileReaderStore.serverRead(fileName());
 					} else {
 						await fileReaderStore.clientRead(selectedFile);
@@ -105,156 +117,168 @@ const ImportJSONFileDialog = observer(({ open, onClose }: Props) => {
 
 	return (
 		<>
-			<Dialog maxWidth="md" onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
+			<Dialog fullWidth={true} maxWidth="lg" onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
 				<DialogTitle id="simple-dialog-title">Import JSON Log</DialogTitle>
 				<div style={{ padding: " 0 1rem 1rem 1rem" }}>
-					<div className="primary-text-color">Tab Name:</div>
-					<input
-						style={{ height: "48px", marginBottom: "1rem" }}
-						className="form-control"
-						placeholder="Tab Name"
-						value={tabName}
-						onChange={(value) => setTabName(value.target.value)} />
-					<hr></hr>
-					<div className="primary-text-color">Import File or Paste Text:</div>
-					<div style={{ display: "flex", alignItems: "center", margin: ".5rem 0 .5rem 0" }}>
-						<button className={'btn btn-primary btn-lg'} style={{ whiteSpace: 'nowrap', marginRight: '.5rem' }}
-							onClick={() => input.click()}
-						>
-							Import File
-						</button>
-						<div style={{ width: "1rem" }}></div>
-						<textarea
+					<div style={{ display: 'flex' }}>
+						<div className="primary-text-color" style={{ whiteSpace: 'nowrap', lineHeight: '48px', marginRight: '.5rem' }}>Tab Name:</div>
+						<input
+							style={{ height: "48px", marginBottom: "1rem" }}
 							className="form-control"
-							style={{ width: "100vw", height: "calc(3 * 48px)", textAlign: "center" }}
-							placeholder="Paste Text Here"
-							value={pastedJSON}
-							onChange={(value) => { setPastedJSON(value.target.value); }} />
+							value={tabName}
+							onChange={(value) => setTabName(value.target.value)} />
 					</div>
-					{selectedFile ? (
-						<>
-							<hr></hr>
-							<span style={{ marginRight: '1rem' }}>{selectedFile.name}</span>
-							<span className="primary-text-color">{displayFileSize(selectedFile.size)}</span>
-							<span style={{ marginLeft: '.5rem', borderRadius: '.5rem', background: isSorted ? 'green' : 'red', color: 'white', padding: '0 .5rem' }}>
-								{isSorted ? 'Sorted' : 'Unsorted'}
-							</span>
-							{subsetSupported &&
+					<TabContext value={tabValue}>
+						<Tabs
+							variant='scrollable'
+							value={tabValue}
+							onChange={(_, v) => setTabValue(v)}
+							textColor="primary"
+							indicatorColor="primary"
+							aria-label="import-tabs"
+						>
+							<Tab value="1" label="Select File" />
+							<Tab value="2" label="Paste JSON" />
+						</Tabs>
+						<TabPanel value="1" key="1">
+							<button className={'btn btn-primary btn-lg'} style={{ whiteSpace: 'nowrap', marginRight: '.5rem' }}
+								onClick={() => input.click()}
+							>
+								Select File
+							</button>
+							{selectedFile ? (
 								<>
 									<hr></hr>
-									<div>
-										<button className="btn btn-lg btn-success"
-											onClick={() => setOpenNewSubsetDialog(true)}
-										>
-											+ New Sorted Subset
-										</button>
-										<div>
-											<FormControl component="fieldset">
-												{/* <FormLabel component="legend">Subset</FormLabel> */}
-												<RadioGroup aria-label="subset" name="subset1"
-													value={selectedSubset}
-													onChange={(e) => setSelectedSubset(e.target.value)}>
-													<FormControlLabel
-														style={{ margin: 0 }}
-														hidden={fileSubsetStore.getSubsets().length === 0}
-														value="none" control={<Radio />}
-														label={
-															<div style={{ display: 'flex' }}>
-																<div style={{ width: '200px', paddingRight: '1rem' }}>
-																	None
-																</div>
-															</div>
-														}
+									<span style={{ marginRight: '1rem' }}>{selectedFile.name}</span>
+									<span className="primary-text-color">{displayFileSize(selectedFile.size)}</span>
+									<span style={{ marginLeft: '.5rem', borderRadius: '.5rem', background: isSorted ? 'green' : 'red', color: 'white', padding: '0 .5rem' }}>
+										{isSorted === true ? 'Sorted' : isSorted === false ? 'Unsorted' : null}
+									</span>
+									{subsetSupported &&
+										<>
+											<hr></hr>
+											<div>
+												<button className="btn btn-lg btn-success"
+													onClick={() => setOpenNewSubsetDialog(true)}
+												>
+													+ New Sorted Subset
+												</button>
+												<div>
+													<FormControl component="fieldset">
+														{/* <FormLabel component="legend">Subset</FormLabel> */}
+														<RadioGroup aria-label="subset" name="subset1"
+															value={selectedSubset}
+															onChange={(e) => setSelectedSubset(e.target.value)}>
+															<FormControlLabel
+																style={{ margin: 0 }}
+																hidden={fileSubsetStore.getSubsets().length === 0}
+																value="none" control={<Radio />}
+																label={
+																	<div style={{ display: 'flex' }}>
+																		<div style={{ width: '200px', paddingRight: '1rem' }}>
+																			None
+																		</div>
+																	</div>
+																}
+															/>
+															{fileSubsetStore.getSubsets().map((subset) => (
+																<FormControlLabel value={subset.filterValue} control={<Radio />}
+																	label={
+																		<div style={{ display: 'flex' }}>
+																			<div
+																				style={{ alignSelf: 'center', verticalAlign: 'middle', marginRight: '.5rem' }}
+																				onClick={() => fileSubsetStore.deleteSubset(selectedFile.name, subset)} title="Delete subset">
+																				<CloseIcon style={{ color: 'red' }} />
+																			</div>
+																			<div style={{ width: '200px', paddingRight: '1rem' }}>
+																				{subset.filterValue}
+																			</div>
+																			<div style={{ width: '100px', paddingRight: '1rem' }}
+																				className="primary-text-color">
+																				{displayFileSize(subset.fileSize)}
+																			</div>
+																			<div>
+																				<span>{dateToHHMMSS(new Date(subset.startTime))}</span>
+																				<span className="primary-text-color"> to </span>
+																				<span>{dateToHHMMSS(new Date(subset.endTime))}</span>
+																			</div>
+																		</div>
+																	} style={{ margin: 0 }} />
+															))}
+														</RadioGroup>
+													</FormControl>
+												</div>
+											</div>
+										</>
+									}
+									{(isSorted || (selectedSubset && selectedSubset !== 'none')) &&
+										<>
+											<hr></hr>
+											<div>
+												<div className="primary-text-color">Time Filter - is rounded down to nearest second:</div>
+												<div style={{ display: 'flex' }}>
+													<input className="form-control" style={{ width: '100%', color: getDateColor(startTime) }}
+														type="text"
+														placeholder="Start time - (e.g., 2024-02-02T12:48:42.125Z)"
+														value={startTime}
+														onChange={(e) => setStartTime(e.target.value)}
 													/>
-													{fileSubsetStore.getSubsets().map((subset) => (
-														<FormControlLabel value={subset.filterValue} control={<Radio />}
-															label={
-																<div style={{ display: 'flex' }}>
-																	<div
-																		style={{ alignSelf: 'center', verticalAlign: 'middle', marginRight: '.5rem' }}
-																		onClick={() => fileSubsetStore.deleteSubset(selectedFile.name, subset)} title="Delete subset">
-																		<CloseIcon style={{ color: 'red' }} />
-																	</div>
-																	<div style={{ width: '200px', paddingRight: '1rem' }}>
-																		{subset.filterValue}
-																	</div>
-																	<div style={{ width: '100px', paddingRight: '1rem' }}
-																		className="primary-text-color">
-																		{displayFileSize(subset.fileSize)}
-																	</div>
-																	<div>
-																		<span>{dateToHHMMSS(new Date(subset.startTime))}</span>
-																		<span className="primary-text-color"> to </span>
-																		<span>{dateToHHMMSS(new Date(subset.endTime))}</span>
-																	</div>
-																</div>
-															} style={{ margin: 0 }} />
-													))}
-												</RadioGroup>
-											</FormControl>
-										</div>
-									</div>
-								</>
-							}
-							{(isSorted || (selectedSubset && selectedSubset !== 'none')) &&
-								<>
+													<div className="primary-text-color" style={{ margin: '0 .5rem', lineHeight: '38px' }}>to</div>
+													<input className="form-control" style={{ width: '100%', color: getDateColor(endTime) }}
+														type="text"
+														placeholder="End time - (e.g., 2024-02-02T12:48:43.356Z)"
+														value={endTime}
+														onChange={(e) => setEndTime(e.target.value)}
+													/>
+												</div>
+											</div>
+										</>
+									}
 									<hr></hr>
-									<div>
-										<div className="primary-text-color">Time Filter - is rounded down to nearest second:</div>
-										<div style={{ display: 'flex' }}>
-											<input className="form-control" style={{ width: '100%', color: getDateColor(startTime) }}
-												type="text"
-												placeholder="Start time - (e.g., 2024-02-02T12:48:42.125Z)"
-												value={startTime}
-												onChange={(e) => setStartTime(e.target.value)}
-											/>
-											<div className="primary-text-color" style={{ margin: '0 .5rem', lineHeight: '38px' }}>to</div>
-											<input className="form-control" style={{ width: '100%', color: getDateColor(endTime) }}
-												type="text"
-												placeholder="End time - (e.g., 2024-02-02T12:48:43.356Z)"
-												value={endTime}
-												onChange={(e) => setEndTime(e.target.value)}
-											/>
-										</div>
+									<div style={{ display: 'flex' }}>
+										<div className="primary-text-color" style={{}}>Operator:</div>
+										<Select
+											value={operator === 'and'
+												? 'and'
+												: 'or'}
+											renderValue={() => <span style={{ color: 'black', marginLeft: '.5rem' }}>
+												{operator === 'and'
+													? <span>AND</span>
+													: <span>OR</span>}
+											</span>}
+											onChange={(e) => setOperator(e.target.value as 'and' | 'or')}
+										>
+											<MenuItem
+												value="and"
+											>
+												<ListItemText primary="AND" />
+											</MenuItem>
+											<MenuItem
+												value="or"
+											>
+												<ListItemText primary="OR" />
+											</MenuItem>
+										</Select>
 									</div>
+									<div className="primary-text-color" style={{}}>Filter:</div>
+									<input className="form-control" style={{ width: '100%' }}
+										type="text"
+										value={includeFilter}
+										onChange={(e) => setIncludeFilter(e.target.value)}
+									></input>
 								</>
+							) : null
 							}
-							<hr></hr>
-							<div style={{ display: 'flex' }}>
-								<div className="primary-text-color" style={{}}>Operator:</div>
-								<Select
-									value={operator === 'and'
-										? 'and'
-										: 'or'}
-									renderValue={() => <span style={{ color: 'black', marginLeft: '.5rem' }}>
-										{operator === 'and'
-											? <span>AND</span>
-											: <span>OR</span>}
-									</span>}
-									onChange={(e) => setOperator(e.target.value as 'and' | 'or')}
-								>
-									<MenuItem
-										value="and"
-									>
-										<ListItemText primary="AND" />
-									</MenuItem>
-									<MenuItem
-										value="or"
-									>
-										<ListItemText primary="OR" />
-									</MenuItem>
-								</Select>
-							</div>
-							<div className="primary-text-color" style={{}}>Filter:</div>
-							<input className="form-control" style={{ width: '100%' }}
-								type="text"
-								value={includeFilter}
-								onChange={(e) => setIncludeFilter(e.target.value)}
-							></input>
-						</>
-					) : null
-					}
-					<hr></hr>
+						</TabPanel>
+						<TabPanel value="2" key="2">
+							<textarea
+								className="form-control"
+								style={{ width: "100%", height: "calc(3 * 48px)" }}
+								placeholder="Paste Text Here"
+								value={pastedJSON}
+								onChange={(value) => { setPastedJSON(value.target.value); }} />
+						</TabPanel>
+					</TabContext>
 					<button className={'btn btn-success btn-lg'} style={{ width: "100%" }}
 						disabled={tabName.length === 0 || (!selectedFile && pastedJSON.length === 0)}
 						onClick={() => setSubmit(true)}
