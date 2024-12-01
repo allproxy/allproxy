f512a04816a4aafe0c00d92dc296bb321bcd39e5
Dave Christenson <Dave.Christenson@ibm.com>
2024-12-01 07:43:52 -0700

    Use prism editor

---
+++ client/src/components/EditorJS.jsx
@@ -0,0 +1,24 @@
+import { Editor } from "prism-react-editor";
+import { BasicSetup } from "prism-react-editor/setups";
+
+// Adding the JSX grammar
+import "prism-react-editor/prism/languages/jsx";
+
+// Adds comment toggling and auto-indenting for JSX
+import "prism-react-editor/languages/jsx";
+
+import "prism-react-editor/layout.css";
+import "prism-react-editor/themes/github-dark.css";
+
+// Required by the basic setup
+import "prism-react-editor/search.css";
+import { jsonLogStore } from "../store/JSONLogStore";
+
+export default function EditorJS() {
+
+	return (
+		<Editor language="jsx" value={jsonLogStore.getScript()} onChange={(e) => jsonLogStore.setScript(e.target.value)} style={{ height: '100vh' }}>
+			{editor => <BasicSetup editor={editor} />}
+		</Editor>
+	);
+}
