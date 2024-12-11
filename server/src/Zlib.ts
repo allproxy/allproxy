f2187469d7c00b0ad726ead64103988a8cae5463
Dave Christenson <Dave.Christenson@ibm.com>
2024-12-10 22:24:31 -0600

    Use pako instead of zlib

--- server/src/Zlib.ts
+++ server/src/Zlib.ts
@@ -1,26 +1,35 @@
-import zlib from 'zlib';
+//import zlib from 'zlib';
+const pako = require('pako');
 
 export function decompressResponse(headers: { [key: string]: any }, data: Buffer): Buffer {
+  //console.log('decompress', headers['content-encoding']);
   if (headers['content-encoding'] === 'gzip') {
     //delete headers['content-encoding'];
-    data = zlib.gunzipSync(data);
+    //data = zlib.gunzipSync(data);
+    data = pako.deflate(data);
   } else if (headers['content-encoding'] === 'br') {
     //delete headers['content-encoding'];
-    data = zlib.brotliDecompressSync(data);
+    //data = zlib.brotliDecompressSync(data);
+    data = pako.deflate(data);
   } else if (headers['content-encoding'] === 'deflate') {
     //delete headers['content-encoding'];
-    data = zlib.deflateSync(data);
+    //data = zlib.deflateSync(data);
+    data = pako.deflate(data);
   }
   return data;
 }
 
 export function compressResponse(headers: { [key: string]: any }, data: Buffer): Buffer {
+  //console.log('compress', headers['content-encoding']);
   if (headers['content-encoding'] === 'gzip') {
-    data = zlib.gzipSync(data);
+    //data = zlib.gzipSync(data);
+    data = pako.gzip(data);
   } else if (headers['content-encoding'] === 'br') {
-    data = zlib.brotliCompressSync(data);
+    ///data = zlib.brotliCompressSync(data);
+    data = pako.inflate(data);
   } else if (headers['content-encoding'] === 'deflate') {
-    data = zlib.inflateSync(data);
+    //data = zlib.inflateSync(data);
+    data = pako.inflate(data);
   }
   return data;
 }
