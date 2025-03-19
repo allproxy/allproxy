5cc0b1621c4a0bccb83e21cd56d1da706e3a2be3
Dave Christenson <Dave.Christenson@ibm.com>
2025-03-19 14:01:01 -0500

    init gtag every hour

--- client/src/GTag.ts
+++ client/src/GTag.ts
@@ -2,6 +2,31 @@ import ReactGA from 'react-ga4';
 
 export default class GTag {
     public static initialize() {
+
+        GTag.doInit();
+
+        setTimeout(() => {
+            async function doPageView() {
+                const { urlPathStore } = await import('./store/UrlPathStore');
+                if (urlPathStore.isGitHubPages()) {
+                    GTag.pageView('Package: Github Pages App');
+                } else {
+                    const { socketStore } = await import('./store/SocketStore');
+                    const type = await socketStore.emitGetInstallType();
+                    GTag.pageView('Package: ' + type);
+                }
+            }
+            doPageView();
+
+            // Initialize gtag once every hour
+            setInterval(() => {
+                GTag.doInit();
+                GTag.pageView('Initializing GTAG every hour');
+            }, 1000 * 60 * 60);
+        }, 1000);
+    }
+
+    private static doInit() {
         const path = document.location.pathname;
         if (path.includes('jsonlogs')) {
             ReactGA.initialize('G-K9M5MG60BK');
@@ -12,38 +37,21 @@ export default class GTag {
         } else {
             ReactGA.initialize('G-H1NDQRZW8J');
         }
-
-        setTimeout(() => {
-            const doPageView = async () => {
-                const { urlPathStore } = await import('./store/UrlPathStore');
-                if (urlPathStore.isGitHubPages()) {
-                    this.pageView('Package: Github Pages App');
-                } else {
-                    const { socketStore } = await import('./store/SocketStore');
-                    const type = await socketStore.emitGetInstallType();
-                    this.pageView('Package: ' + type);
-                }
-            };
-            doPageView();
-        }, 1000);
     }
 
     public static pageView(title: string) {
-        if (!ReactGA.isInitialized) this.initialize();
-        ReactGA.event('page_view', {
-            page_title: title
-        });
+        setTimeout(() => ReactGA.event('page_view', {
+            page_title: process.env.REACT_APP_VERSION + ' ' + title
+        }));
     }
 
     public static search(searchTerm: string) {
-        if (!ReactGA.isInitialized) this.initialize();
         setTimeout(() => ReactGA.event('search', {
             search_term: searchTerm
         }));
     }
 
     public static exception(description: string, fatal: boolean) {
-        if (!ReactGA.isInitialized) this.initialize();
         setTimeout(() => ReactGA.event('exception', {
             description, fatal
         }));
